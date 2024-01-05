1d8f1129fe281690d83f697579bd130915a5afe2
SiddarthBaruah <95840068+SiddarthBaruah@users.noreply.github.com>
2024-01-05 10:31:47 +0530

    Update left_bar.dart

--- lib/widgets/left_bar.dart
+++ lib/widgets/left_bar.dart
@@ -1,26 +1,22 @@
-import 'package:firstapp/constants/app_constants.dart';
+import 'package:bmi_calculator/constants/app_constants.dart';
 import 'package:flutter/material.dart';
 
 class LeftBar extends StatelessWidget {
+  const LeftBar({super.key, required this.barWidth});
   final double barWidth;
-
-  const LeftBar({Key? key, required this.barWidth}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.end,
       children: [
         Container(
-          height: 25,
-          width: barWidth,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(20),
-                bottomLeft: Radius.circular(20),
-              ),
-              color: accentHexColor),
-        ),
+            height: 25,
+            width: barWidth,
+            decoration: const BoxDecoration(
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(20),
+                    topLeft: Radius.circular(20))),
+            color: accentHexColor)
       ],
     );
   }

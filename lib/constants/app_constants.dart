357ac3762437093650fc5952297e1ab3ada2ec1a
SiddarthBaruah <95840068+SiddarthBaruah@users.noreply.github.com>
2024-01-05 10:30:58 +0530

    Update app_constants.dart

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -4,7 +4,7 @@ class HexColor extends Color {
   static int _getColorFromHex(String hexColor) {
     hexColor = hexColor.toUpperCase().replaceAll('#', '');
     if (hexColor.length == 6) {
-      hexColor = 'FF' + hexColor;
+      hexColor = "FF$hexColor";
     }
     return int.parse(hexColor, radix: 16);
   }
@@ -12,6 +12,5 @@ class HexColor extends Color {
   HexColor(final String hexColor) : super(_getColorFromHex(hexColor));
 }
 
-final Color mainHexColor= HexColor('#1C1C1C');
-final Color accentHexColor= HexColor("#FCC91C");
-
+final Color mainHexColor = HexColor('#1C1C1C');
+final Color accentHexColor = HexColor('#FCC91C');

6089cc8a3df5b2abc73d076c39687c5eef2c23ab
SiddarthBaruah <95840068+SiddarthBaruah@users.noreply.github.com>
2024-01-05 10:32:30 +0530

    Update right_bar.dart

--- lib/widgets/right_bar.dart
+++ lib/widgets/right_bar.dart
@@ -1,26 +1,22 @@
-import 'package:firstapp/constants/app_constants.dart';
+import 'package:bmi_calculator/constants/app_constants.dart';
 import 'package:flutter/material.dart';
 
 class RightBar extends StatelessWidget {
+  const RightBar({super.key, required this.barWidth});
   final double barWidth;
-
-  const RightBar({Key? key, required this.barWidth}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.start,
       children: [
         Container(
-          height: 25,
-          width: barWidth,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.only(
-                topRight: Radius.circular(20),
-                bottomRight: Radius.circular(20),
-              ),
-              color: accentHexColor),
-        ),
+            height: 25,
+            width: barWidth,
+            decoration: const BoxDecoration(
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(20),
+                    topLeft: Radius.circular(20))),
+            color: accentHexColor)
       ],
     );
   }
